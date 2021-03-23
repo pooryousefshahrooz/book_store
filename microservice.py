41f2cd8d95534588fcbae01c78096d5897d4978c
Shahrooz <sh.pooryousef2@gmail.com>
2021-03-22 21:56:21 -0400

    a simple hello world microservice with flask

---
+++ microservice.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+  return "Hello, World!"
+
+
+# In[ ]:
+
+
+
+
